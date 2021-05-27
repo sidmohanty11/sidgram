4e926b7c1707d56a7a743f9f840277de5d42f13a
sidmohanty11 <sidmohanty11@gmail.com>
2021-05-28 00:40:18 +0530

    working on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,69 @@
-import React, {useContext} from 'react';
-import { FirebaseContext } from '../context/firebase';
+import React, { useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
 import { UserContext } from "../context/user";
+import * as ROUTES from "../constants/routes";
+import { Link } from "react-router-dom";
+
 const Header = () => {
-    const { firebase } = useContext(FirebaseContext);
-    const { user } = useContext(UserContext);
-    return (
-        <div>
-            
+  const { firebase } = useContext(FirebaseContext);
+  const { user } = useContext(UserContext);
+  return (
+    <div className="h-16 bg-white border-b border-gray-primary mb-8">
+      <div className="container mx-auto max-w-screen-lg  h-full">
+        <div className="flex justify-between h-full">
+          <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
+            <h1 className="flex justify-center w-full">
+              <Link to={ROUTES.DASHBOARD}>
+                <img
+                  className="mt-2 w-2/12"
+                  src="./images/instagram-logo1.png"
+                  alt="Instagram"
+                />
+              </Link>
+            </h1>
+          </div>
+          <div className="text-gray-700 text-center flex items-center align-items">
+            {user ? (
+              <>
+                <Link to={ROUTES.DASHBOARD}>
+                  <svg className="w-8 mr-6 cursor-pointer"
+                    height="512pt"
+                    viewBox="0 0 512 512"
+                    width="512pt"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="m498.195312 222.695312c-.011718-.011718-.023437-.023437-.035156-.035156l-208.855468-208.847656c-8.902344-8.90625-20.738282-13.8125-33.328126-13.8125-12.589843 0-24.425781 4.902344-33.332031 13.808594l-208.746093 208.742187c-.070313.070313-.140626.144531-.210938.214844-18.28125 18.386719-18.25 48.21875.089844 66.558594 8.378906 8.382812 19.445312 13.238281 31.277344 13.746093.480468.046876.964843.070313 1.453124.070313h8.324219v153.699219c0 30.414062 24.746094 55.160156 55.167969 55.160156h81.710938c8.28125 0 15-6.714844 15-15v-120.5c0-13.878906 11.289062-25.167969 25.167968-25.167969h48.195313c13.878906 0 25.167969 11.289063 25.167969 25.167969v120.5c0 8.285156 6.714843 15 15 15h81.710937c30.421875 0 55.167969-24.746094 55.167969-55.160156v-153.699219h7.71875c12.585937 0 24.421875-4.902344 33.332031-13.808594 18.359375-18.371093 18.367187-48.253906.023437-66.636719zm0 0" />
+                  </svg>
+                </Link>
+                <button
+                  type="button"
+                  title="SignOut"
+                  onClick={() => firebase.auth().signOut()}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      firebase.auth().signOut();
+                    }
+                  }}
+                >
+                  <svg className="w-8 mr-6 cursor-pointer"
+                    height="512pt"
+                    viewBox="0 0 512.00533 512"
+                    width="512pt"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="m320 277.335938c-11.796875 0-21.332031 9.558593-21.332031 21.332031v85.335937c0 11.753906-9.558594 21.332032-21.335938 21.332032h-64v-320c0-18.21875-11.605469-34.496094-29.054687-40.554688l-6.316406-2.113281h99.371093c11.777344 0 21.335938 9.578125 21.335938 21.335937v64c0 11.773438 9.535156 21.332032 21.332031 21.332032s21.332031-9.558594 21.332031-21.332032v-64c0-35.285156-28.714843-63.99999975-64-63.99999975h-229.332031c-.8125 0-1.492188.36328175-2.28125.46874975-1.027344-.085937-2.007812-.46874975-3.050781-.46874975-23.53125 0-42.667969 19.13281275-42.667969 42.66406275v384c0 18.21875 11.605469 34.496093 29.054688 40.554687l128.386718 42.796875c4.351563 1.34375 8.679688 1.984375 13.226563 1.984375 23.53125 0 42.664062-19.136718 42.664062-42.667968v-21.332032h64c35.285157 0 64-28.714844 64-64v-85.335937c0-11.773438-9.535156-21.332031-21.332031-21.332031zm0 0" />
+                    <path d="m505.75 198.253906-85.335938-85.332031c-6.097656-6.101563-15.273437-7.9375-23.25-4.632813-7.957031 3.308594-13.164062 11.09375-13.164062 19.714844v64h-85.332031c-11.777344 0-21.335938 9.554688-21.335938 21.332032 0 11.777343 9.558594 21.332031 21.335938 21.332031h85.332031v64c0 8.621093 5.207031 16.40625 13.164062 19.714843 7.976563 3.304688 17.152344 1.46875 23.25-4.628906l85.335938-85.335937c8.339844-8.339844 8.339844-21.824219 0-30.164063zm0 0" />
+                  </svg>
+                </button>
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
