08177c1f3c39da932d3da57f3508b94563063147
sidmohanty11 <sidmohanty11@gmail.com>
2021-05-27 21:12:30 +0530

    the login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from './constants/routes';
+
+const Login = lazy(() => import("./pages/login"));
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route exact path={ROUTES.LOGIN}>
+            <Login />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
