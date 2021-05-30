76fb431ed6502c7ca7a643b7e892a1e7fdac4ee7
sidmohanty11 <sidmohanty11@gmail.com>
2021-05-30 12:07:46 +0530

    fixing up

--- src/pages/notfound.js
+++ src/pages/notfound.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import Header from '../components/Header';
 
 const Notfound = () => {
     useEffect(() => {
@@ -7,6 +8,7 @@ const Notfound = () => {
 
     return (
         <div className="bg-gray-background">
+            <Header />
             <div className="mx-auto max-w-screen-lg">
                 <p className="text-center text-2xl">Not Found!</p>
             </div>
